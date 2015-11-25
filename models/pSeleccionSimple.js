6846f97eb15c08bfe0808000192ce1364c63f1ed
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-11-25 14:34:13 +0000

    new attributes

--- models/pSeleccionSimple.js
+++ models/pSeleccionSimple.js
@@ -2,9 +2,11 @@
 
 var Pregunta = require('../models/preguntas.js');
 
-function PSSimple(enunciado){
+function PSSimple(enunciado, arrayResp){
   Pregunta.call(this,enunciado);
-  
+  this.arrayResp = arrayResp;
+  this.entrada = "";
+
 }
 
 module.exports = PSSimple;
