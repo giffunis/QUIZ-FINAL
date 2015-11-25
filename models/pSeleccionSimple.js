2d424c51b496bd4d994382295c5c987a70930a99
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-11-25 17:38:52 +0000

    working on pSimple view

--- models/pSeleccionSimple.js
+++ models/pSeleccionSimple.js
@@ -5,8 +5,6 @@ var Pregunta = require('../models/preguntas.js');
 function PSSimple(enunciado, arrayResp){
   Pregunta.call(this,enunciado);
   this.arrayResp = arrayResp;
-  this.entrada = "";
-
 }
 
 module.exports = PSSimple;
