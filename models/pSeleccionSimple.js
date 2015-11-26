206e1abbdf25f473406cddb2798c18b68d11c7b6
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-11-26 16:32:47 +0000

    Now I use dynamic includes with ejs and it work only for pSeleccionSymple

--- models/pSeleccionSimple.js
+++ models/pSeleccionSimple.js
@@ -1,12 +1,18 @@
-"use stric";
+
 
 var Pregunta = require('../models/preguntas.js');
 
 function PSSimple(enunciado, arrayResp){
   Pregunta.call(this,enunciado);
   this.arrayResp = arrayResp;
-  this.entrada = "eval('<% include ../../partials/preguntas/pSimple %>')";
+  this.entrada = '../../partials/preguntas/pSimple';
   this.tipo = "PSSimple";
+
+
 }
+
+
+
+
 
 module.exports = PSSimple;
