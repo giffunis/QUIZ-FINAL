12361488529cc751bb94f668071549c2f47cbfd9
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-12-09 19:16:09 +0000

    solved a view error

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -48,7 +48,7 @@ exports.create = function(req, res) {
   var quiz = models.Quiz.build(req.body.quiz);
   quiz.validate().then(function(err){
     if(err){
-      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      res.render('pages/quizes/new', {quiz: quiz, errors: err.errors});
     }else {
       quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
         res.redirect('/quizes');
