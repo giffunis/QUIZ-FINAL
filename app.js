0a8983938bf6bbca296dff1be515dcef10cc2b32
José Saúl Giffuni <giffunis@gmail.com>
2015-12-28 15:41:50 +0000

    added a helper dinamic to redirect path

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ var app = express();
 
 app.set('layout', 'layout'); // defaults to 'layout'
 
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -32,8 +29,17 @@ app.use(session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
+// Helpers dinamic
+app.use(function(req,res,next){
+  if(!req.path.match(/\/login|\/logout/)){
+    req.session.redir = req.path;
+  }
+  // para hacer visible a las listas la variable req.session
+  res.locals.session = req.session;
+  next();
+});
 
-
+// Routes
 app.use('/', routes);
 app.use('/quizes',quizesRoute);
 
