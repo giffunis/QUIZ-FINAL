f8fe386de16cf982bba9bec7d378f6b2b53208c5
José Saúl Giffuni <giffunis@gmail.com>
2015-12-28 16:37:26 +0000

    create function done

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -5,3 +5,18 @@ exports.new = function(req, res){
   req.session.errors = {};
   res.render('pages/login', {errors: errors});
 };
+
+exports.create = function(req,res){
+  var login = req.body.login;
+  var password = req.body.password;
+  var userController = require('./user_controller');
+  userController.autenticar(login,password, function(error, user){
+    if(error){
+      req.session.errors = [{"message": 'Se ha producido un error: ' + error}];
+      redirect('/login');
+      return;
+    }
+    req.session.user = {id: user.id, username: user.username};
+    res.redirect(req.session.redir.toString());
+  });
+};
