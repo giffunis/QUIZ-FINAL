4d15ce16c20c2158c090d3fdb11e2f9442b55373
José Saúl Giffuni <giffunis@gmail.com>
2015-12-28 19:58:33 +0000

    controller create  user created

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,6 @@
-var users = { admin: {id:1, username:"admin", password:"1234"}};
+var models = require('../models/models.js');
+
+// var users = { admin: {id:1, username:"admin", password:"1234"}};
 
 exports.autenticar = function(login, password, callback){
   if (users[login]) {
@@ -10,4 +12,22 @@ exports.autenticar = function(login, password, callback){
   } else {
     callback(new Error('No existe ese usuario'));
   }
+};
+
+exports.create = function(req, res){
+  var user = models.User.build(
+    {
+      username: req.body.user.username,
+      password: req.body.user.password
+    });
+
+  user.validate().then(function(err){
+    if(err){
+      res.render('pages/user/new', {user: user, errors: err.errors});
+    }else{
+      user.save().then(function(){
+        res.redirect('/login');
+      });
+    }
+  }).catch();
 };
