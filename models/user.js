f9f62ebc39fcd6aeb9dff56b68dbbd9f5459974e
José Saúl Giffuni <giffunis@gmail.com>
2015-12-28 18:49:55 +0000

    created a new user table

---
+++ models/user.js
@@ -0,0 +1,14 @@
+
+module.exports = function(sequelize, DataTypes){
+  // sequelize.define(table_name, estructura);
+  return sequelize.define('User',{
+    username: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: "El nombre de usuario no puede estár vacío."}}
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: "Falta la contraseña"}}
+    }
+  });
+};
