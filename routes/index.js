edd8507c82f6635bcfc0b2725f56504a38adb211
José Saúl Giffuni <giffunis@gmail.com>
2015-12-28 15:59:52 +0000

    I need to create a new controller to session

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', quizController.home);
+
+// ------------------------------ SESSION ROUTES -------------------------------
+/* GET login */
+router.get('/login', sessionController.new);
+/* POST login */
+
+/* DELETE login */
+
 
 module.exports = router;
