6692c58a1b98666dab4af2eb95a1be39eaca7dc3
José Saúl Giffuni <giffunis@gmail.com>
2015-12-28 17:47:12 +0000

    login required created to verify if you are logged or not

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,13 @@
 // var models = require('../models/models.js');
 
+exports.loginRequired = function(req,res,next){
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
 exports.new = function(req, res){
   var errors = req.session.errors || {};
   req.session.errors = {};
