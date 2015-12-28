85df00500816ce18d20554034a54b89c1bb16ac3
José Saúl Giffuni <giffunis@gmail.com>
2015-12-28 10:21:13 +0000

    npm install method-override

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 var quizesRoute = require('./routes/quizes.js');
@@ -28,6 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 
 app.use('/', routes);
