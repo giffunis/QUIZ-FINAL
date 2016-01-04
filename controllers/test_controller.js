20b93a56b16e9739723bec8d252f01965189ce2c
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-01-04 22:20:28 +0000

    now the home controller works, but it is not completed

--- controllers/test_controller.js
+++ controllers/test_controller.js
@@ -1,8 +1,8 @@
 var models = require('../models/models.js');
 
 exports.home = function(req, res, next){
-  models.Quiz.findAll({order:['id','DESC']}).then(function(quizes){
-    res.render('/pages/test/home', {quizes: quizes});
+  models.Quiz.findAll({order:'`id` DESC'}).then(function(quizes){
+    res.render('pages/test/index', {quizes: quizes});
   }).catch(function(error){
     next(new Error(error));
   });
