2ce2adb3f3772ecc5b22eab0d354232cf655a85d
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-01-05 19:24:33 +0000

    added a route to an user.autoload and user.perfil

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 var quizController = require('../controllers/quiz_controller');
 var sessionController = require('../controllers/session_controller');
 var userController = require('../controllers/user_controller');
 var testController = require('../controllers/test_controller');
 
+quizesRoute.param('quizId', quizController.load);
 /* GET home page. */
 router.get('/', quizController.home);
 
@@ -22,6 +22,7 @@ router.get('/user', userController.index);
 router.get('/user/new', userController.new);
 router.post('/user/new', userController.create);
 router.get('/user/show', sessionController.loginRequired, userController.show);
+router.get('/user/show/:userId', sessionController.loginRequired, userController.perfil);
 
 // ------------------------------ TEST ROUTES -------------------------------
 router.get('/test', testController.home);
