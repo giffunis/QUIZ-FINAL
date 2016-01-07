240471516b351e1bc2e054e154947233ccbff172
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-01-07 21:19:51 +0000

    BestScore updated

--- controllers/test_controller.js
+++ controllers/test_controller.js
@@ -17,6 +17,16 @@ exports.control = function(req, res){
     if(req.quiz.id < quizes.length){
       res.render('pages/test/index', {quizes: quizes, idQ: req.quiz.id});
     } else {
+      if(req.session.user.score > req.session.user.bestScore){
+        req.session.user.bestScore = req.session.user.score;
+        models.User.find({where:{username: req.session.user.username}}).then(function(user){
+          user.bestScore = req.session.user.bestScore;
+          user.save();
+          res.render('pages/quizes/index', {quizes: quizes});
+        }).catch(function(error){
+          next(new Error(error));
+        });
+      }
       res.render('pages/index', {title: req.session.user.score});
     }
   }).catch(function(error){
